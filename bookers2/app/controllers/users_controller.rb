a339dbdbe3b1cdeb98832d0b4af0f6e71906c222
Egyh <124222570+Egyh@users.noreply.github.com>
2023-02-20 12:20:43 +0000

    profile image update

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -19,12 +19,12 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to books_path(user_params)
+    redirect_to user_path(user_params)
   end
 
   private
 
   def user_params
-  params.require(:user).permit(:name, :introduction)
+  params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
